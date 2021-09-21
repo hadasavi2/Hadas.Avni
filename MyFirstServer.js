8beaad19244078e536dbacc59c67888f0955c5c9
Hadas <hadasav@post.bgu.ac.il>
2021-09-21 22:06:23 +0300

    server changes add DB MySQL

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,9 +1,37 @@
 const http = require('http');
+const mysql = require ('mysql')
+const express = require ("express")
+const bodyParser = require ("body-parser")
 
-const requestListener = function (req, res) {
+
+const app = express()
+
+app.get("/",(req, res)=> {
   res.writeHead(200,{'Content-Type':'text/html'});
-  res.end('Hello, World! - Hadas made it');
-}
+  res.end('Hello, World! - Hadas made it 2');
+})
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+var connection = mysql.createConnection({
+  host:'localhost',
+  user: 'root',
+  password:'221993',
+  database:'mysql'
+})
+connection.connect(error=>{
+  if (error){
+    throw error
+  }
+  console.log('Hadas here')
+})
+app.get("/customers",function(req,res){
+  connection.query ("SELECT * FROM customers" ,function (err, rows, fields){
+    if(err) 
+      throw err
+    console.log (`The solution is: ${JSON.stringify(rows)} 0 solution`)
+    res.send(JSON.stringify(rows))
+  })
+})
+
+app.listen(8080,()=>{
+  console.log ('Hadas2')
+})
