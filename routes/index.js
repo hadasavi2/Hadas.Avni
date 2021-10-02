af285de4de5e1ec4be9231831bc0e9b5f36a85b3
hadas <hadas.avni2@gmail.com>
2021-10-02 22:20:12 +0300

    make the node js return the html files

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+const path = require("path");
+var router = express.Router();
+
+/* GET home page.
+router.get('/', function(req, res, next) {
+  res.render('myFirstHtml.html', { title: 'Express' });
+});
+
+router.get('/cv',(req,res) => {
+  res.sendFile(path.join(__dirname,'../','views','/CV.html'));
+});
+
+router.get('/*',(req,res) => {
+  res.sendFile(path.join(__dirname,'../','views','/myFirstHtml.html'));
+});
+
+
+module.exports = router;
+*/
